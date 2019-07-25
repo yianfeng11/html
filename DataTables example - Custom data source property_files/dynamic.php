980954e47ba9f5fc8824b7d2f1055964b1746359
Ian Feng <ianfeng@datarget.com.tw>
2019-07-25 16:45:15 +0800

    html

---
+++ DataTables example - Custom data source property_files/dynamic.php
@@ -0,0 +1,8 @@
+window._site.forumUser = "";
+$(document).ready( function () {
+	window._site.comments( $('div.content div.comments'), [] );
+} );window._site.page = "examples\/ajax\/custom_data_property.html";
+
+$(document).ready( function () {
+	window._site.dynamicLoaded();
+} );
